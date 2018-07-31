187265d2382cfbd3f6db708095af3bddbd77fead
Chad Eylander <eylanderchad@gmail.com>
2018-07-31 11:47:10 -0700

    added find characers

---
+++ findCharacters/findCharacters.py
@@ -0,0 +1,18 @@
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']
+
+
+print(word_list[0][0])
+
+
+
+
+def findCharacter(arg):
+    new_List = []
+    for index in arg:
+        if  isinstance(index, str):
+            for indexNum in arg:
+                if index = 'o';
