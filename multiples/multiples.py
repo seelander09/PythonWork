d36194c27e32a290361ae936a99c5fed2e65a80b
Chad Eylander <eylanderchad@gmail.com>
2018-08-03 16:54:32 -0700

    added multiples

---
+++ multiples/multiples.py
@@ -0,0 +1,41 @@
+'''
+Assignment: Multiples, Sum, Average
+This assignment has several parts. All of your code should be in one file
+that is well commented to indicate what each block of code is doing and
+which problem you are solving. Don't forget to test your code as you go!
+
+Multiples
+Part I - Write code that prints all the odd numbers from 1 to 1000.
+Use the for loop and don't use a list to do this exercise.
+
+Part II - Create another program that prints
+all the multiples of 5 from 5 to 1,000,000.
+
+Sum List
+Create a program that prints the sum of all the values
+in the list: a = [1, 2, 5, 10, 255, 3]
+
+Average List
+Create a program that prints the average
+of the values in the list: a = [1, 2, 5, 10, 255, 3]
+'''
+
+list = range(0,1000)
+for i in list:
+    if i % 2 != 0:
+        print (i)
+
+
+list = range(5,100000)
+for i in list:
+    if i % 5 == 0:
+        print (i)
+
+
+list = [1, 2, 5, 10, 255, 3]
+sum(list)
+print(sum(list))
+
+lst = [1, 2, 5, 10, 255, 3]
+average = sum(lst)/len(lst)
+print (average)
