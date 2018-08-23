9961a3a0a2141ea15a015174edab707c0e3c5eef
Chad Eylander <eylanderchad@gmail.com>
2018-08-22 21:52:40 -0700

    added stars

---
+++ DrawingStars/drawingStars.py
@@ -0,0 +1,16 @@
+'''
+Create a function called draw_stars() that takes a list of numbers and prints out *.
+
+For example:
+
+x = [4, 6, 1, 3, 5, 7, 25]
+draw_stars(x) should print the following when invoked:
+
+****
+******
+*
+***
+*****
+*******
+*************************
+'''
